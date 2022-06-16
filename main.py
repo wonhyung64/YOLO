5e96ddf98d4c169fb351013de78e275a46e2e0e3
an-seunghwan <wonhyung64@icloud.com>
2022-06-16 23:17:57 +0900

    feat - boxprior

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #%%
+import os
 import numpy as np
 from sklearn.cluster import KMeans
 from tqdm import tqdm
@@ -12,17 +13,64 @@ from utils import (
     preprocess,
 )
 import tensorflow_datasets as tfds
-#%%
-data_dir = "D:/won/data/tfds"
-img_size = (416, 416)
-batch_size=4
-name = "voc/2007"
 
-datasets, labels, data_num = load_dataset(name=name, data_dir=data_dir)
-train_set, valid_set, test_set = build_dataset(datasets, data_num, batch_size, img_size)
-print(next(train_set))
 
-# %%
+def load_box_prior(dataset, img_size, data_num, k_per_grid=3):
+    box_prior_dir = f".data_chkr/{''.join(char for char in name if char.isalnum())}_box_prior.csv"
+    if not(os.path.exists(box_prior_dir)):
+        box_prior = build_box_prior(dataset, img_size, data_num, k_per_grid)
+        box_prior.to_csv(box_prior_dir, index=False, header=False)
+
+
+def build_box_prior(dataset, img_size, data_num, k_per_grid):
+    gt_hws = collect_boxes(dataset, data_num, img_size)
+    hw_area = gt_hws[...,0] * gt_hws[...,1]
+    hw1 = gt_hws[hw_area <= np.quantile(hw_area, 0.333333)]
+    hw2 = gt_hws[np.logical_and(
+        hw_area > np.quantile(hw_area, 0.333333),
+        hw_area <= np.quantile(hw_area, 0.666666)
+        )]
+    hw3 = gt_hws[hw_area > np.quantile(hw_area, 0.666666)]
+
+    box_prior = []
+    progress = tqdm(range(3))
+    progress.set_description(
+        "Clustering boxes"
+    )
+    for i in progress:
+        cluster_sample = (hw1, hw2, hw3)[i]
+        box_prior.append(k_means(cluster_sample, k_per_grid))
+    box_prior = np.concatenate(box_prior)
+
+    prior_df = pd.DataFrame(box_prior, columns=["height", "width"] )
+    prior_df.insert(2, "area", box_prior[..., 0] * box_prior[..., 1])
+    prior_df = prior_df.sort_values(by=["area"], axis=0)
+    prior_df = prior_df[["height", "width"]]
+
+    return prior_df
+
+
+def k_means(cluster_sample, k_per_grid):
+    model = KMeans(n_clusters = k_per_grid, random_state = 1)
+    model.fit(cluster_sample)
+    box_prior = model.cluster_centers_
+
+    return box_prior
+
+
+def collect_boxes(dataset, data_num, img_size):
+    prior_samples = []
+    progress = tqdm(range(data_num))
+    progress.set_description(
+        "Collecting boxes extraced from gt_boxes"
+    )
+    for _ in progress:
+        image, gt_boxes, gt_labels = next(dataset)
+        prior_sample = extract_boxes(gt_boxes, img_size)
+        prior_samples.append(prior_sample)
+    gt_hws = tf.concat(prior_samples, axis=0)
+    
+    return gt_hws
 
 
 def extract_boxes(gt_boxes, img_size):
@@ -37,43 +85,20 @@ def extract_boxes(gt_boxes, img_size):
 
     return prior_sample
 
+#%%
+if __name__ == "__main__":
+    os.makedirs("data_chkr", exist_ok=True)
+    data_dir = "D:/won/data/tfds"
+    img_size = (416, 416)
+    batch_size=4
+    name = "voc/2012"
 
-def k_means(cluster_sample, k_per_grid):
-    model = KMeans(n_clusters = k_per_grid, random_state = 1)
-    model.fit(cluster_sample)
-    box_prior = model.cluster_centers_
+    datasets, labels, data_num = load_dataset(name=name, data_dir=data_dir)
+    train_set, valid_set, test_set = build_dataset(datasets, batch_size, img_size)
 
-    return box_prior
+    load_box_prior(train_set, img_size, data_num)
 
 #%%
-# k_per_grid = 3
-# # def build_box_prior(dataset, img_size, k_per_grid):
-# prior_samples = []
-# # for _ in tqdm(range(data_num)):
-# for _ in tqdm(range(10)):
-#     image, gt_boxes, _ = next(train_set)
-#     prior_sample = extract_boxes(gt_boxes, img_size)
-#     prior_samples.append(prior_sample)
-# gt_hws = tf.concat(prior_samples, axis=0)
-# del prior_samples
 
-# hw_area = gt_hws[...,0] * gt_hws[...,1]
-# hw1 = gt_hws[hw_area <= np.quantile(hw_area, 0.333333)]
-# hw2 = gt_hws[np.logical_and(
-#     hw_area > np.quantile(hw_area, 0.333333),
-#     hw_area <= np.quantile(hw_area, 0.666666)
-#     )]
-# hw3 = gt_hws[hw_area > np.quantile(hw_area, 0.666666)]
-
-# box_prior = []
-# for cluster_sample in (hw1, hw2, hw3):
-#     box_prior.append(k_means(cluster_sample, k_per_grid))
-# box_prior = np.concatenate(box_prior)
-
-# prior_df = pd.DataFrame(box_prior, columns=["height", "width"] )
-# prior_df.insert(2, "area", box_prior[..., 0] * box_prior[..., 1])
-# prior_df = prior_df.sort_values(by=["area"], axis=0)
-# name = ''.join(filter(str.isalnum, name)) 
-# prior_df[["height", "width"]].to_csv(f'./box_prior_tmp{name}.csv', header=False, index=False)
 
 # %%
