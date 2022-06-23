38cce68489a12fba7d3e67b942dd070449e72ea7
an-seunghwan <wonhyung64@icloud.com>
2022-06-23 15:30:25 +0900

    feat - reconstruction

--- main.py
+++ main.py
@@ -8,37 +8,39 @@ from utils import (
     build_anchor_ops,
     build_target,
     yolo_v3,
-    loss_fn,
+    build_optimizer,
+    forward_backward,
 )
 
 #%%
 if __name__ == "__main__":
     os.makedirs("data_chkr", exist_ok=True)
     data_dir = "D:/won/data/tfds"
-    img_size = (416, 416)
-    batch_size = 4
+    img_size = [416, 416]
+    batch_size = 16
     name = "coco/2017"
+    epochs = 160
+    lambda_dict = {
+        "lambda_yx": 1e-1,
+        "lambda_hw": 1e-5,
+        "lambda_obj": 1e-1,
+        "lambda_nobj": 1e-4,
+        "lambda_cls": 1e-3,
+        }
+
 
     datasets, labels, data_num = load_dataset(name=name, data_dir=data_dir)
     train_set, valid_set, test_set = build_dataset(datasets, batch_size, img_size)
     box_priors = load_box_prior(train_set, name, img_size, data_num)
     anchors, prior_grids, offset_grids, stride_grids = build_anchor_ops(img_size, box_priors)
-    model = yolo_v3((416,416,3), labels, offset_grids, prior_grids, fine_tunning=True)
+    model = yolo_v3(img_size+[3], labels, offset_grids, prior_grids, fine_tunning=True)
+    optimizer = build_optimizer(batch_size, data_num)
 
     for _ in tqdm(range(data_num)):
         image, gt_boxes, gt_labels = next(train_set)
-        pred_yx, pred_hw, pred_obj, pred_cls = model(image)
-        true_yx, true_hw, true_obj, true_nobj, true_cls = build_target(anchors, gt_boxes, gt_labels, labels, img_size, stride_grids)
-        yx_loss, hw_loss, obj_loss, nobj_loss, cls_loss = loss_fn(
-            pred=[pred_yx, pred_hw, pred_obj, pred_cls],
-            true=[true_yx, true_hw, true_obj, true_nobj, true_cls],
-            batch_size=batch_size
-        )
-        print(yx_loss)
-        print(hw_loss)
-        print(obj_loss)
-        print(nobj_loss)
-        print(cls_loss)
+        true = build_target(anchors, gt_boxes, gt_labels, labels, img_size, stride_grids)
+        loss = forward_backward(image, true, model, optimizer, batch_size, lambda_dict.values())
+        print(loss)
         
         if _ == 100: break 
 # %%
