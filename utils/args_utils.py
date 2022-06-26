2a2f855558d846ea0a92498f5d54c6bb08c9f0cb
an-seunghwan <wonhyung64@icloud.com>
2022-06-26 20:11:18 +0900

    feat - add argparse utils

--- utils/args_utils.py
+++ utils/args_utils.py
@@ -7,7 +7,7 @@ def build_args():
     parser.add_argument("--data-dir", type = str, default="D:/won/data/tfds")
     parser.add_argument("--img-size", nargs="+", type=int, default=[416, 416])
     parser.add_argument("--batch-size", type = int, default=16)
-    parser.add_argument("--dataset-name", type = str, default="voc/2007")
+    parser.add_argument("--name", type = str, default="voc/2007")
     parser.add_argument("--lambda-yx", type = float, default=1e-1)
     parser.add_argument("--lambda-hw", type = float, default=1e-5)
     parser.add_argument("--lambda-obj", type = float, default=1e-1)
