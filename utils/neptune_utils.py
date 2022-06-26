3f990d7cf2b57c4309317a95c5fbec00b6e00b6f
an-seunghwan <wonhyung64@icloud.com>
2022-06-26 20:11:44 +0900

    feat - add neptune_utils.py

---
+++ utils/neptune_utils.py
@@ -0,0 +1,30 @@
+import os
+try: import neptune.new as neptune
+except:
+    import sys
+    import subprocess
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "neptune-client"])
+    import neptune.new as neptune
+
+
+def plugin_neptune(NEPTUNE_API_KEY, NEPTUNE_PROJECT, args):
+    os.environ["NEPTUNE_API_TOKEN"] = NEPTUNE_API_KEY
+
+    run = neptune.init(project=NEPTUNE_PROJECT,
+                       api_token=NEPTUNE_API_KEY,
+                       mode="offline",
+                       )
+
+    run["sys/name"] = "yolo-optimization"
+    run["sys/tags"].add([f"{key}: {value}" for key, value in args._get_kwargs()])
+
+    return run
+
+
+def record_train_loss(run, loss, total_loss):
+    run["train/loss/yx_loss"].log(loss[0].numpy())
+    run["train/loss/hw_loss"].log(loss[1].numpy())
+    run["train/loss/obj_loss"].log(loss[2].numpy())
+    run["train/loss/nobj_loss"].log(loss[3].numpy())
+    run["train/loss/cls_loss"].log(loss[4].numpy())
+    run["train/loss/total_loss"].log(total_loss.numpy())
