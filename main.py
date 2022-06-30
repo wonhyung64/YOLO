900ae4d8e61b0b24eeba656c8506064ef784998c
an-seunghwan <wonhyung64@icloud.com>
2022-06-30 17:48:47 +0900

    feat - reconstruction

--- main.py
+++ main.py
@@ -18,148 +18,87 @@ from utils import (
     delta_to_bbox,
     decode_pred,
     draw_output,
+    calculate_iou,
+    calculate_ap_const,
     NEPTUNE_API_KEY,
     NEPTUNE_PROJECT,
 )
 
 
 
+def validation(valid_set, model):
+    map = []
+    validation_progress = tqdm(range(100))
+    for _ in validation_progress:
+        image, gt_boxes, gt_labels = next(valid_set)
+        pred = model(image)
+        pred = [tf.stack([pred[0][...,1], pred[0][...,0]], axis=-1), pred[1], pred[2], pred[3]]
+        final_bboxes, final_labels, final_scores = decode_pred(pred, stride_grids)
+        ap = calculate_ap_const(final_bboxes, final_labels, gt_boxes, gt_labels, labels)
+        validation_progress.set_description("Validation | Average_Precision {:.4f}".format(ap))
+        map.append(ap)
+
+    map_res = tf.reduce_mean(map)
+
+    return map_res
+
+
+
+
 
 #%%
 if __name__ == "__main__":
-    # os.makedirs("data_chkr", exist_ok=True)
-    # args = build_args()
-    # run = plugin_neptune(NEPTUNE_API_KEY, NEPTUNE_PROJECT, args)
+    os.makedirs("data_chkr", exist_ok=True)
+    args = build_args()
+    run = plugin_neptune(NEPTUNE_API_KEY, NEPTUNE_PROJECT, args)
 
-    # lambda_lst = build_lambda(args)
-    # datasets, labels, data_num = load_dataset(name=args.name, data_dir=args.data_dir)
-    # train_set, valid_set, test_set = build_dataset(datasets, args.batch_size, args.img_size)
-    # box_priors = load_box_prior(train_set, args.name, args.img_size, data_num)
-    # anchors, prior_grids, offset_grids, stride_grids = build_anchor_ops(args.img_size, box_priors)
-    # model = yolo_v3(args.img_size+[3], labels, offset_grids, prior_grids, fine_tunning=True)
-    # optimizer = build_optimizer(args.batch_size, data_num)
+    lambda_lst = build_lambda(args)
+    datasets, labels, data_num = load_dataset(name=args.name, data_dir=args.data_dir)
+    train_set, valid_set, test_set = build_dataset(datasets, args.batch_size, args.img_size)
+    box_priors = load_box_prior(train_set, args.name, args.img_size, data_num)
+    anchors, prior_grids, offset_grids, stride_grids = build_anchor_ops(args.img_size, box_priors)
+    model = yolo_v3(args.img_size+[3], labels, offset_grids, prior_grids, fine_tunning=True)
+    optimizer = build_optimizer(args.batch_size, data_num)
 
-    # for epoch in range(1, args.epochs+1):
-    #     epoch_progress = tqdm(range(data_num//args.batch_size))
-    #     for _ in epoch_progress:
-    #         image, gt_boxes, gt_labels = next(train_set)
-    #         true = build_target(anchors, gt_boxes, gt_labels, labels, args.img_size, stride_grids)
-    #         loss, total_loss = forward_backward(image, true, model, optimizer, args.batch_size, lambda_lst)
-    #         record_train_loss(run, loss, total_loss)
-    #         epoch_progress.set_description(
-    #             "Epoch {}/{} | yx {:.4f}, hw {:.4f}, obj {:.4f}, nobj {:.4f}, cls {:.4f}, total {:.4f}".format(
-    #                 epoch,
-    #                 args.epochs,
-    #                 loss[0].numpy(),
-    #                 loss[1].numpy(),
-    #                 loss[2].numpy(),
-    #                 loss[3].numpy(),
-    #                 loss[4].numpy(),
-    #                 total_loss.numpy(),
-    #             )
-    #         )
+    best_map = 0
+    for epoch in range(1, args.epochs+1):
+        epoch_progress = tqdm(range(data_num//args.batch_size))
+        for _ in epoch_progress:
+            image, gt_boxes, gt_labels = next(train_set)
+            true = build_target(anchors, gt_boxes, gt_labels, labels, args.img_size, stride_grids)
+            loss, total_loss = forward_backward(image, true, model, optimizer, args.batch_size, lambda_lst)
+            record_train_loss(run, loss, total_loss)
+            epoch_progress.set_description(
+                "Epoch {}/{} | yx {:.4f}, hw {:.4f}, obj {:.4f}, nobj {:.4f}, cls {:.4f}, total {:.4f}".format(
+                    epoch,
+                    args.epochs,
+                    loss[0].numpy(),
+                    loss[1].numpy(),
+                    loss[2].numpy(),
+                    loss[3].numpy(),
+                    loss[4].numpy(),
+                    total_loss.numpy(),
+                )
+            )
+        map_res = validation(valid_set, model)
+
+        run["validation/mAP"].log(map_res.numpy())
+
+        if map_res.numpy() > best_map:
+            best_map = map_res.numpy()
+            ckpt_dir = f"model_ckpt/{dataset_name}/rpn_weights"
+            rpn_model.save_weights(f"{ckpt_dir}/weights")
+            ckpt = os.listdir(ckpt_dir)
+            for i in range(len(ckpt)):
+                run[f"{ckpt_dir}/{ckpt[i]}"].upload(f"{ckpt_dir}/{ckpt[i]}")
+
 
     datasets, labels, data_num = load_dataset(name="coco/2017", data_dir="D:/won/data/tfds")
     train_set, valid_set, test_set = build_dataset(datasets, 1, [416,416])
     box_priors = load_box_prior(train_set, "coco/2017", [416,416], data_num)
     anchors, prior_grids, offset_grids, stride_grids = build_anchor_ops([416,416], box_priors)
-    model = tf.keras.models.load_model("tmp.h5")
+    model = yolo_v3([416,416,3], labels, offset_grids, prior_grids, fine_tunning=True)
+    model.load_weights("C:/Users/USER/Documents/GitHub/YOLO/yolov3_org_weights/weights")
     
 
-    for _ in tqdm(range(100)):
-        image, gt_boxes, gt_labels = next(test_set)
-        final_bboxes, final_labels, final_scores = decode_pred(model(image))
-        draw_output(image, final_bboxes, final_labels, final_scores, labels)
-
 # %%
-
-import numpy as np
-import tensorflow as tf
-from .bbox_utils import generate_iou
-
-
-def calculate_PR(final_bbox, gt_box, mAP_threshold):
-    bbox_num = final_bbox.shape[1]
-    gt_num = gt_box.shape[1]
-
-    true_pos = tf.Variable(tf.zeros(bbox_num))
-    for i in range(bbox_num):
-        bbox = tf.split(final_bbox, bbox_num, axis=1)[i]
-
-        iou = generate_iou(bbox, gt_box)
-
-        best_iou = tf.reduce_max(iou, axis=1)
-        pos_num = tf.cast(tf.greater(best_iou, mAP_threshold), dtype=tf.float32)
-        if tf.reduce_sum(pos_num) >= 1:
-            gt_box = gt_box * tf.expand_dims(
-                tf.cast(1 - pos_num, dtype=tf.float32), axis=-1
-            )
-            true_pos = tf.tensor_scatter_nd_update(true_pos, [[i]], [1])
-    false_pos = 1.0 - true_pos
-    true_pos = tf.math.cumsum(true_pos)
-    false_pos = tf.math.cumsum(false_pos)
-
-    recall = true_pos / gt_num
-    precision = tf.math.divide(true_pos, true_pos + false_pos)
-
-    return precision, recall
-
-
-def calculate_AP_per_class(recall, precision):
-    interp = tf.constant([i / 10 for i in range(0, 11)])
-    AP = tf.reduce_max(
-        [tf.where(interp <= recall[i], precision[i], 0.0) for i in range(len(recall))],
-        axis=0,
-    )
-    AP = tf.reduce_sum(AP) / 11
-    return AP
-
-
-def calculate_AP_const(
-    final_bboxes, final_labels, gt_boxes, gt_labels, hyper_params, mAP_threshold=0.5
-):
-    total_labels = hyper_params["total_labels"]
-    AP = []
-    for c in range(1, total_labels):
-        if tf.math.reduce_any(final_labels == c) or tf.math.reduce_any(gt_labels == c):
-            final_bbox = tf.expand_dims(final_bboxes[final_labels == c], axis=0)
-            gt_box = tf.expand_dims(gt_boxes[gt_labels == c], axis=0)
-
-            if final_bbox.shape[1] == 0 or gt_box.shape[1] == 0:
-                ap = tf.constant(0.0)
-            else:
-                precision, recall = calculate_PR(final_bbox, gt_box, mAP_threshold)
-                ap = calculate_AP_per_class(recall, precision)
-            AP.append(ap)
-    if AP == []:
-        AP = 1.0
-    else:
-        AP = tf.reduce_mean(AP)
-    return AP
-
-
-def calculate_AP(final_bboxes, final_labels, gt_boxes, gt_labels, hyper_params):
-    total_labels = hyper_params["total_labels"]
-    mAP_threshold_lst = np.arange(0.5, 1.0, 0.05)
-    APs = []
-    for mAP_threshold in mAP_threshold_lst:
-        AP = []
-        for c in range(1, total_labels):
-            if tf.math.reduce_any(final_labels == c) or tf.math.reduce_any(
-                gt_labels == c
-            ):
-                final_bbox = tf.expand_dims(final_bboxes[final_labels == c], axis=0)
-                gt_box = tf.expand_dims(gt_boxes[gt_labels == c], axis=0)
-
-                if final_bbox.shape[1] == 0 or gt_box.shape[1] == 0:
-                    ap = tf.constant(0.0)
-                else:
-                    precision, recall = calculate_PR(final_bbox, gt_box, mAP_threshold)
-                    ap = calculate_AP_per_class(recall, precision)
-                AP.append(ap)
-        if AP == []:
-            AP = 1.0
-        else:
-            AP = tf.reduce_mean(AP)
-        APs.append(AP)
-    return tf.reduce_mean(APs)
