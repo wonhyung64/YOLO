031b7e6c1a802ef255c99364b81a9649276a04bc
an-seunghwan <wonhyung64@icloud.com>
2022-07-01 21:42:47 +0900

    feat - reconstruction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #%%
 import os
+import time
 import tensorflow as tf
+import neptune.new as neptune
 from tqdm import tqdm
 from utils import (
     load_dataset,
@@ -15,54 +17,33 @@ from utils import (
     build_lambda,
     plugin_neptune,
     record_train_loss,
-    delta_to_bbox,
     decode_pred,
     draw_output,
-    calculate_iou,
     calculate_ap_const,
+    sync_neptune,
     NEPTUNE_API_KEY,
     NEPTUNE_PROJECT,
 )
 
 
-
-def validation(valid_set, model):
-    map = []
-    validation_progress = tqdm(range(100))
-    for _ in validation_progress:
-        image, gt_boxes, gt_labels = next(valid_set)
-        pred = model(image)
-        pred = [tf.stack([pred[0][...,1], pred[0][...,0]], axis=-1), pred[1], pred[2], pred[3]]
-        final_bboxes, final_labels, final_scores = decode_pred(pred, stride_grids)
-        ap = calculate_ap_const(final_bboxes, final_labels, gt_boxes, gt_labels, labels)
-        validation_progress.set_description("Validation | Average_Precision {:.4f}".format(ap))
-        map.append(ap)
-
-    map_res = tf.reduce_mean(map)
-
-    return map_res
-
-
-
-
-
-#%%
-if __name__ == "__main__":
-    os.makedirs("data_chkr", exist_ok=True)
-    args = build_args()
-    run = plugin_neptune(NEPTUNE_API_KEY, NEPTUNE_PROJECT, args)
-
-    lambda_lst = build_lambda(args)
-    datasets, labels, data_num = load_dataset(name=args.name, data_dir=args.data_dir)
-    train_set, valid_set, test_set = build_dataset(datasets, args.batch_size, args.img_size)
-    box_priors = load_box_prior(train_set, args.name, args.img_size, data_num)
-    anchors, prior_grids, offset_grids, stride_grids = build_anchor_ops(args.img_size, box_priors)
-    model = yolo_v3(args.img_size+[3], labels, offset_grids, prior_grids, fine_tunning=True)
-    optimizer = build_optimizer(args.batch_size, data_num)
-
-    best_map = 0
-    for epoch in range(1, args.epochs+1):
-        epoch_progress = tqdm(range(data_num//args.batch_size))
+def train(
+    run,
+    args,
+    train_num,
+    train_set,
+    valid_set,
+    labels,
+    anchors,
+    stride_grids,
+    model,
+    optimizer,
+    lambda_lst,
+    weights_dir
+    ):
+    best_mean_ap = 0
+    start_time = time.time()
+    for epoch in range(args.epochs):
+        epoch_progress = tqdm(range(train_num//args.batch_size))
         for _ in epoch_progress:
             image, gt_boxes, gt_labels = next(train_set)
             true = build_target(anchors, gt_boxes, gt_labels, labels, args.img_size, stride_grids)
@@ -70,8 +51,8 @@ if __name__ == "__main__":
             record_train_loss(run, loss, total_loss)
             epoch_progress.set_description(
                 "Epoch {}/{} | yx {:.4f}, hw {:.4f}, obj {:.4f}, nobj {:.4f}, cls {:.4f}, total {:.4f}".format(
-                    epoch,
-                    args.epochs,
+                    epoch+1,
+                    args.epochs+1,
                     loss[0].numpy(),
                     loss[1].numpy(),
                     loss[2].numpy(),
@@ -80,25 +61,88 @@ if __name__ == "__main__":
                     total_loss.numpy(),
                 )
             )
-        map_res = validation(valid_set, model)
+        mean_ap = validation(valid_set, stride_grids, model, labels)
 
-        run["validation/mAP"].log(map_res.numpy())
+        run["validation/mAP"].log(mean_ap.numpy())
 
-        if map_res.numpy() > best_map:
-            best_map = map_res.numpy()
-            ckpt_dir = f"model_ckpt/{dataset_name}/rpn_weights"
-            rpn_model.save_weights(f"{ckpt_dir}/weights")
-            ckpt = os.listdir(ckpt_dir)
-            for i in range(len(ckpt)):
-                run[f"{ckpt_dir}/{ckpt[i]}"].upload(f"{ckpt_dir}/{ckpt[i]}")
+        # if mean_ap.numpy() > best_mean_ap:
+        #     best_mean_ap = mean_ap.numpy()
+        model.save_weights(weights_dir)
+        run["model"].upload(weights_dir)
+
+    train_time = time.time() - start_time
+
+    return train_time
 
 
-    datasets, labels, data_num = load_dataset(name="coco/2017", data_dir="D:/won/data/tfds")
-    train_set, valid_set, test_set = build_dataset(datasets, 1, [416,416])
-    box_priors = load_box_prior(train_set, "coco/2017", [416,416], data_num)
-    anchors, prior_grids, offset_grids, stride_grids = build_anchor_ops([416,416], box_priors)
-    model = yolo_v3([416,416,3], labels, offset_grids, prior_grids, fine_tunning=True)
-    model.load_weights("C:/Users/USER/Documents/GitHub/YOLO/yolov3_org_weights/weights")
-    
+def validation(valid_set, stride_grids, model, labels):
+    aps = []
+    validation_progress = tqdm(range(100))
+    for _ in validation_progress:
+        image, gt_boxes, gt_labels = next(valid_set)
+        pred = model(image)
+        final_bboxes, final_labels, final_scores = decode_pred(pred, stride_grids)
+        ap = calculate_ap_const(final_bboxes, final_labels, gt_boxes, gt_labels, labels)
+        validation_progress.set_description("Validation | Average_Precision {:.4f}".format(ap))
+        aps.append(ap)
 
-# %%
+    mean_ap = tf.reduce_mean(aps)
+
+    return mean_ap
+
+
+def test(run, test_num, test_set, model, stride_grids, labels):
+    test_times = []
+    aps = []
+    test_progress = tqdm(range(test_num))
+    for step in test_progress:
+        image, gt_boxes, gt_labels = next(test_set)
+        start_time = time.time()
+        pred = model(image)
+        final_bboxes, final_labels, final_scores = decode_pred(pred, stride_grids)
+        test_time = time.time() - start_time
+        ap = calculate_ap_const(final_bboxes, final_labels, gt_boxes, gt_labels, labels)
+        test_progress.set_description("Test | Average_Precision {:.4f}".format(ap))
+        aps.append(ap)
+        test_times.append(test_time)
+
+        if step <= 20 == 0:
+            run["outputs"].log(
+                neptune.types.File.as_image(
+                    draw_output(image, final_bboxes, final_labels, final_scores, labels)
+                    )
+            )
+
+    mean_ap = tf.reduce_mean(aps)
+    mean_test_time = tf.reduce_mean(test_times)
+
+    return mean_ap, mean_test_time
+
+
+#%%
+if __name__ == "__main__":
+    args = build_args()
+    run = plugin_neptune(NEPTUNE_API_KEY, NEPTUNE_PROJECT, args)
+
+    experiment_name = run.get_url().split("/")[1]
+    experiment_dir = "./model_weights/experiment"
+    weights_dir = f"{experiment_dir}/{experiment_name}.h5"
+
+    os.makedirs(experiment_dir, exist_ok=True)
+    os.makedirs("./data_chkr", exist_ok=True)
+
+    lambda_lst = build_lambda(args)
+    datasets, labels, train_num, test_num = load_dataset(name=args.name, data_dir=args.data_dir)
+    train_set, valid_set, test_set = build_dataset(datasets, args.batch_size, args.img_size)
+    box_priors = load_box_prior(train_set, args.name, args.img_size, train_num)
+    anchors, prior_grids, offset_grids, stride_grids = build_anchor_ops(args.img_size, box_priors)
+    model = yolo_v3(args.img_size+[3], labels, offset_grids, prior_grids, fine_tunning=True)
+    optimizer = build_optimizer(args.batch_size, train_num)
+
+    train_time = train(run, args, train_num, train_set, valid_set, labels, anchors, stride_grids, model, optimizer, lambda_lst, weights_dir)
+
+    model.load_weights(weights_dir)
+    mean_ap, mean_test_time = test(run, test_num, test_set, model, stride_grids, labels)
+
+    sync_neptune(run, experiment_name, mean_ap, train_time, mean_test_time, NEPTUNE_API_KEY, NEPTUNE_PROJECT)
+
