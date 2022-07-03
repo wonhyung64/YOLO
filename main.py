86c65ae146eb8ede04fcc8d4ea8d07338035ab4f
an-seunghwan <wonhyung64@icloud.com>
2022-07-03 21:03:19 +0900

    feat - mirrored distributed strategy

--- main.py
+++ main.py
@@ -37,7 +37,8 @@ def train(
     model,
     optimizer,
     lambda_lst,
-    weights_dir
+    weights_dir,
+    strategy
     ):
     best_mean_ap = 0
     start_time = time.time()
@@ -46,7 +47,10 @@ def train(
         for _ in epoch_progress:
             image, gt_boxes, gt_labels = next(train_set)
             true = build_target(anchors, gt_boxes, gt_labels, labels, args.img_size, stride_grids)
-            loss, total_loss = forward_backward(image, true, model, optimizer, args.batch_size, lambda_lst)
+            loss = strategy.run(forward_backward, args=(image, true, model, optimizer, args.batch_size, lambda_lst,))
+            if args.data_dir != "D:/won/data":
+                loss = [strategy.reduce(tf.distribute.ReduceOp.MEAN, loss[i], axis=0) for i in range(len(loss))]
+            total_loss = tf.reduce_sum(loss)
             record_train_loss(run, loss, total_loss)
             epoch_progress.set_description(
                 "Epoch {}/{} | yx {:.4f}, hw {:.4f}, obj {:.4f}, nobj {:.4f}, cls {:.4f}, total {:.4f}".format(
@@ -120,6 +124,7 @@ def test(run, test_num, test_set, model, stride_grids, labels):
 def main():
     args = build_args()
     run = plugin_neptune(NEPTUNE_API_KEY, NEPTUNE_PROJECT, args)
+    mirrored_strategy = tf.distribute.MirroredStrategy()
 
     experiment_name = run.get_url().split("/")[1]
     experiment_dir = "./model_weights/experiment"
@@ -130,13 +135,14 @@ def main():
 
     lambda_lst = build_lambda(args)
     datasets, labels, train_num, test_num = load_dataset(name=args.name, data_dir=args.data_dir)
-    train_set, valid_set, test_set = build_dataset(datasets, args.batch_size, args.img_size)
+    train_set, valid_set, test_set = build_dataset(datasets, args.batch_size, args.img_size, mirrored_strategy)
     box_priors = load_box_prior(train_set, args.name, args.img_size, train_num)
     anchors, prior_grids, offset_grids, stride_grids = build_anchor_ops(args.img_size, box_priors)
-    model = yolo_v3(args.img_size+[3], labels, offset_grids, prior_grids, args.data_dir, fine_tunning=True)
-    optimizer = build_optimizer(args.batch_size, train_num)
-
-    train_time = train(run, args, train_num, train_set, valid_set, labels, anchors, stride_grids, model, optimizer, lambda_lst, weights_dir)
+    with mirrored_strategy.scope():
+        model = yolo_v3(args.img_size+[3], labels, offset_grids, prior_grids, args.data_dir, fine_tunning=True)
+        optimizer = build_optimizer(args.batch_size, train_num)
+    with mirrored_strategy.scope():
+        train_time = train(run, args, train_num, train_set, valid_set, labels, anchors, stride_grids, model, optimizer, lambda_lst, weights_dir, mirrored_strategy)
 
     model.load_weights(weights_dir)
     mean_ap, mean_test_time = test(run, test_num, test_set, model, stride_grids, labels)
